7871f1d4fa1153856045504fb93011ad4a6b3e9f
jalyseh <hammettjadyn@gmail.com>
2020-11-05 22:18:49 -0500

    fifth commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product'; 
 
 
 @Injectable()
@@ -10,11 +11,16 @@ export class ProductService {
 
 
 private _albumUrl: string = '../assets/album.json';
+private _productsUrl: string = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum (id: number) : Observable<Album>{
     return this._http.get(this._albumUrl).map((response) => <Album> response.json());
+  }
+
+  getProducts() : Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response) => <Product[]> response.json());
   }
 }
 
